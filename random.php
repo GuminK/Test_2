1c8d17f2518fb04baa23aa673b4c169411221ad7
GuminK <kimgumin9284@gmail.com>
2023-09-14 15:40:42 +0900

    first commit

---
+++ random.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ko">
+  <head?>
+    <meta charset="utf-8">
+    <title>무작위 숫자 생성</title>
+  </head>
+  <body>
+    <p>1에서 100까지 무작위 숫자 생성하기:
+      <?php
+
+      echo rand(1,100);
+
+      ?>
+  </body>
+</html>
